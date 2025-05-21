0e73487d4d2d50d025a3cf8fd3f1d7cb8c0e24d2
Vimal Kumar <Vimal2540.vk@gmail.com>
2025-05-21 14:43:25 +0530

    Displaying Comments

---
+++ client/src/CommentList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const CommentList = ({ postId }) => {
+  const [comments, setComments] = useState([]);
+  console.log(postId);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${postId}/comments`
+    );
+
+    console.log(res);
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map(comment => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
