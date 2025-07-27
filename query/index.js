7c09dc4613d48c97fcf30bd2ed82c30f6160eb26
Vimal Kumar <Vimal2540.vk@gmail.com>
2025-07-27 17:30:20 +0530

    Parsing Incoming Events

--- query/index.js
+++ query/index.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+const posts = {};
 
-app.post('/events', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(4002, async () => {
   console.log('Listening on 4002');
-  try {
-    const res = await axios.get('http://localhost:4005/events');
+  //   try {
+  //     const res = await axios.get('http://localhost:4005/events');
 
-    for (let event of res.data) {
-      console.log('Processing event:', event.type);
+  //     for (let event of res.data) {
+  //       console.log('Processing event:', event.type);
 
-      handleEvent(event.type, event.data);
-    }
-  } catch (error) {
-    console.log(error.message);
-  }
+  //       handleEvent(event.type, event.data);
+  //     }
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
 });
