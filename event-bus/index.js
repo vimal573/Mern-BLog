4ba9d5062eeb51739c552e1c08c79f9f2fbb29be
Vimal Kumar <Vimal2540.vk@gmail.com>
2025-07-31 10:03:53 +0530

    Dockering Other Services

--- event-bus/index.js
+++ event-bus/index.js
@@ -24,6 +24,13 @@ app.post('/events', (req, res) => {
     console.log(err.message);
   });
 
+  axios.post('http://localhost:4006/events', event).catch(err => {
+    console.log(err.message);
+  });
+  axios.post('http://localhost:4007/events', event).catch(err => {
+    console.log(err.message);
+  });
+
   res.send({ status: 'OK' });
 });
 
